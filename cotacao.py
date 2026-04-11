06afc7a440adafd8f551d6ba221b0000d7e5b59e
Alex <alex-fjp@hotmail.com>
2026-04-11 00:09:12 -0300

    refatora método buscar_cotacoes para melhorar a legibilidade e tratamento de erros

--- cotacao.py
+++ cotacao.py
@@ -5,24 +5,18 @@ class CotacaoMoedaConverter:
         pass
 
     def buscar_cotacoes(self):
-        """
-        Consome a AwesomeAPI para buscar cotações de moedas e cripto.
-        Retorna um dicionário com os valores de compra (bid).
-        """
-        # A URL já pede especificamente Dólar, Euro e Bitcoin contra o Real
+
         url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 
-        # Fazendo a requisição GET para a API
         response = requests.get(url)
         
-        # O raise_for_status() é uma boa prática! Ele gera um erro se a API estiver fora do ar
-        response.raise_for_status()  
+        if response.status_code != 200:
+            raise ValueError("Erro ao buscar cotações")
         
-        # Convertendo a resposta da API (que vem em JSON) para um dicionário Python
+        # Dicionário em Python
         dados = response.json()
         
-        # Extraindo apenas o valor atual (bid) e formatando
-        # Convertendo a string que a API devolve para float (número decimal)
+        # Convertendo a string que a API devolve para número decimal
         cotacoes = {
             "Dólar": float(dados["USDBRL"]["bid"]),
             "Euro": float(dados["EURBRL"]["bid"]),
