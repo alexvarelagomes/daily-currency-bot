1e02b5c80701be2caa5ec374bc8ccff821b58687
Alex <alex-fjp@hotmail.com>
2026-04-11 00:11:55 -0300

    ✨ feat: remove comentários desnecessários e melhora a clareza do código

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from cotacao import CotacaoMoedaConverter
 
-# Bloco para testar se o nosso script está funcionando
 if __name__ == "__main__":
     conversor = CotacaoMoedaConverter()
     valores_hoje = conversor.buscar_cotacoes()
 
     if valores_hoje:
         for moeda, valor in valores_hoje.items():
-            # Formatando para mostrar 2 casas decimais, padrão de dinheiro
             print(f"{moeda}: R$ {valor:.2f}")
